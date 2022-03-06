2243ce156ec19c1af6f8f61a5630e637649f0f74
Gokulrajv1304 <gvenugo1@my.centennialcollege.ca>
2022-03-05 20:26:07 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Gokulraj:EBdMavrqiB8yYa9M@mongodbserver.is4xt.mongodb.net/WebAppDev?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+  //"URI": "mongodb://localhost/books229"
+};
