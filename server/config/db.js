84bef3c7e0707670ff036a85981ca15bad0e10b9
Gokulrajv1304 <gvenugo1@my.centennialcollege.ca>
2022-03-24 18:02:06 -0400

    Added Comments

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ Date: 05-March-2022
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://Gokulraj:EBdMavrqiB8yYa9M@mongodbserver.is4xt.mongodb.net/WebAppDev?retryWrites=true&w=majority",
+  "URI": "mongodb+srv://Gokulraj:euAEY9OOn4826LIY@mongodbserver.is4xt.mongodb.net/WebAppDev?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
   //"URI": "mongodb://localhost/books229"
 };
